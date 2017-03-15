474a1d2af771fbde5d022b4ceda7036973f036d6
Moushumi Seal <mseal@Moushumis-MacBook-Pro.local>
2017-03-15 08:36:42 +0800

    Initial commit for personal and healthbio

--- app/src/main/java/sg/edu/nus/iss/se/ft05/medipal/activities/PersonalBioActivity.java
+++ app/src/main/java/sg/edu/nus/iss/se/ft05/medipal/activities/PersonalBioActivity.java
@@ -1,18 +1,15 @@
 package sg.edu.nus.iss.se.ft05.medipal.activities;
 
-import android.app.DatePickerDialog;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Locale;
 
 import sg.edu.nus.iss.se.ft05.medipal.R;
@@ -24,25 +21,23 @@ import sg.edu.nus.iss.se.ft05.medipal.fragments.DatePickerFragment;
  */
 public class PersonalBioActivity extends AppCompatActivity {
 
-    private EditText name, dob, idNo, address, postalCode, height;
-    private Spinner spn_bloodType;
-    private Button saveBtn;
+    private EditText mName, mDob, mIdNo, mAddress, mPostalCode, mHeight;
+    private Spinner mSpn_bloodType;
+    private Button mSaveBtn;
     private SimpleDateFormat dateFormatter = new SimpleDateFormat("dd MMM yyyy", Locale.getDefault());
-    Calendar selectedDate = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_bio);
-        name = (EditText)findViewById(R.id.name);
-        dob = (EditText)findViewById(R.id.dob);
-        idNo = (EditText)findViewById(R.id.idNo);
+        mName = (EditText)findViewById(R.id.name);
+        mDob = (EditText)findViewById(R.id.dob);
+        mIdNo = (EditText)findViewById(R.id.idNo);
 
-        spn_bloodType = (Spinner) findViewById(R.id.spn_bloodType);
-        spn_bloodType.setAdapter(
+        mSpn_bloodType = (Spinner) findViewById(R.id.spn_bloodType);
+        mSpn_bloodType.setAdapter(
                 new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, BloodType.values()));
-        dob.setText(dateFormatter.format(selectedDate.getTime()));
-        dob.setOnClickListener(new View.OnClickListener() {
+        mDob.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DialogFragment newFragment = new DatePickerFragment();
--- app/src/main/java/sg/edu/nus/iss/se/ft05/medipal/fragments/DatePickerFragment.java
+++ app/src/main/java/sg/edu/nus/iss/se/ft05/medipal/fragments/DatePickerFragment.java
@@ -3,13 +3,17 @@ package sg.edu.nus.iss.se.ft05.medipal.fragments;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
@@ -18,30 +22,32 @@ import java.util.Calendar;
 import sg.edu.nus.iss.se.ft05.medipal.R;
 
 
-public class DatePickerFragment extends DialogFragment
-        implements DatePickerDialog.OnDateSetListener {
+public class DatePickerFragment extends DialogFragment{
 
-    private Calendar selectedDate;
+    private DatePicker mDatePicker;
+    private Button mCancelBtn, mSetBtn;
+
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current date as the default date in the picker
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        getDialog().getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        return inflater.inflate(R.layout.datepicker_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mDatePicker = (DatePicker) view.findViewById(R.id.picker);
+        setPicker();
+    }
+
+    private void setPicker(){
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
-
-        // Create a new instance of DatePickerDialog and return it
-        return new DatePickerDialog(getActivity(), this, year, month, day);
-    }
-
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, dayOfMonth);
-        selectedDate = calendar;
-    }
-
-    public Calendar getSelectedDate(){
-        return selectedDate;
+        mDatePicker.updateDate(year, month, day);
     }
 }
