87854e73ddb0df90485a155b3d3178cb7b2d6ab3
Ashish Shrikant Katre <e0146812@nus.edu.sg>
2017-03-24 11:09:50 +0800

    drag drop improvement

--- app/src/main/java/sg/edu/nus/iss/se/ft05/medipal/listeners/ContactsTouchHelperCallback.java
+++ app/src/main/java/sg/edu/nus/iss/se/ft05/medipal/listeners/ContactsTouchHelperCallback.java
@@ -2,7 +2,10 @@ package sg.edu.nus.iss.se.ft05.medipal.listeners;
 
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
 import android.widget.Toast;
+
+import org.apache.http.conn.ConnectTimeoutException;
 
 import sg.edu.nus.iss.se.ft05.medipal.adapters.ItemTouchHelperAdapter;
 
@@ -14,6 +17,8 @@ import sg.edu.nus.iss.se.ft05.medipal.adapters.ItemTouchHelperAdapter;
 public class ContactsTouchHelperCallback extends ItemTouchHelper.Callback {
 
     private final ItemTouchHelperAdapter mAdapter;
+    private int dragfrom = -1;
+    private int dragto = -1;
 
     public ContactsTouchHelperCallback(ItemTouchHelperAdapter adapter) {
 
@@ -26,17 +31,38 @@ public class ContactsTouchHelperCallback extends ItemTouchHelper.Callback {
                           RecyclerView.ViewHolder contactsViewHolder,
                           RecyclerView.ViewHolder contactsViewHolderTarget) {
 
-        mAdapter.onItemMove(contactsViewHolder.getAdapterPosition(),
-                contactsViewHolderTarget.getAdapterPosition());
+        int fromPosition = contactsViewHolder.getAdapterPosition();
+        int toPosition = contactsViewHolderTarget.getAdapterPosition();
+
+
+        if (dragfrom == -1) {
+
+            dragfrom = fromPosition;
+        }
+
+        dragto = toPosition;
 
         return true;
     }
 
     @Override
+    public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        super.clearView(recyclerView, viewHolder);
+
+        if (dragfrom != -1 && dragto != -1 && dragfrom != dragto) {
+
+            mAdapter.onItemMove(dragfrom, dragto);
+        }
+
+        dragfrom = dragto = -1;
+    }
+
+/*    @Override
     public boolean isLongPressDragEnabled() {
 
         return true;
-    }
+    }*/
+
 
     @Override
     public int interpolateOutOfBoundsScroll(RecyclerView recyclerView, int viewSize, int viewSizeOutOfBounds, int totalSize, long msSinceStartScroll) {
@@ -54,9 +80,19 @@ public class ContactsTouchHelperCallback extends ItemTouchHelper.Callback {
     @Override
     public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder contactsViewHolder) {
 
-        int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+     /*   int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
         int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
 
+        return makeMovementFlags(dragFlags, swipeFlags);*/
+
+//        return makeFlag(ItemTouchHelper.ACTION_STATE_DRAG,
+//                ItemTouchHelper.DOWN | ItemTouchHelper.UP);
+
+//        return makeFlag(ItemTouchHelper.ACTION_STATE_IDLE, ItemTouchHelper.UP | ItemTouchHelper.DOWN) | makeFlag(ItemTouchHelper.ACTION_STATE_DRAG,
+//                ItemTouchHelper.DOWN | ItemTouchHelper.UP);
+
+        int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN | ItemTouchHelper.START | ItemTouchHelper.END;
+        int swipeFlags = 0;
         return makeMovementFlags(dragFlags, swipeFlags);
     }
 
