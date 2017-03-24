28cf5c4624a937ead04811e63a1e000aef7f444c
maakashdeep <maakashdeep@gmail.com>
2017-03-24 16:31:51 +0800

    Added help to onOptionsItemSelected

--- app/src/main/java/sg/edu/nus/iss/se/ft05/medipal/activities/MainActivity.java
+++ app/src/main/java/sg/edu/nus/iss/se/ft05/medipal/activities/MainActivity.java
@@ -102,6 +102,10 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.action_settings) {
             return true;
         }
+        else if(id == R.id.action_help) {
+            setFragment(new HelpFragment());
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
