fe047b208375d9ac8e60c191d2d39f3a940eb5b0
Moushumi Seal <mseal@Moushumis-MacBook-Pro.local>
2017-03-24 15:16:33 +0800

    welcome screen

---
+++ app/src/main/java/sg/edu/nus/iss/se/ft05/medipal/activities/SplashActivity.java
@@ -0,0 +1,35 @@
+package sg.edu.nus.iss.se.ft05.medipal.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import sg.edu.nus.iss.se.ft05.medipal.R;
+
+/**
+ * @author Moushumi Seal
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    /**
+     * Duration of wait
+     */
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_screen);
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
