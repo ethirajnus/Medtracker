717c75c48598cc4ba724c7aa307d0d359f5cfa59
ethiraj <ethirajsrinivasan@gmail.com>
2017-03-24 22:19:04 +0800

    Consumption Unconsumed

--- app/src/main/java/sg/edu/nus/iss/se/ft05/medipal/fragments/ConsumptionFragment.java
+++ app/src/main/java/sg/edu/nus/iss/se/ft05/medipal/fragments/ConsumptionFragment.java
@@ -42,40 +42,6 @@ public class ConsumptionFragment extends Fragment {
         ((MainActivity) getActivity()).setFloatingActionButtonAction(AddOrUpdateConsumption.class);
         RecyclerView consumptionRecyclerView;
         context = getActivity().getApplicationContext();
-//        consumptionRecyclerView = (RecyclerView) view.findViewById(R.id.all_consumption_list_view);
-//        // Set layout for the RecyclerView, because it's a list we are using the linear layout
-//        consumptionRecyclerView.setLayoutManager(new LinearLayoutManager(context));
-//
-//        // Get all guest info from the database and save in a cursor
-//        Cursor cursor = Consumption.findAll(context);
-//
-//        // Create an adapter for that cursor to display the data
-//        mAdapter = new ConsumptionListAdapter(context, cursor);
-//
-//        // Link the adapter to the RecyclerView
-//        consumptionRecyclerView.setAdapter(mAdapter);
-//
-//        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-//
-//            @Override
-//            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-//                //do nothing, we only care about swiping
-//                return false;
-//            }
-//
-//            @Override
-//            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
-//                //get the id of the item being swiped
-//                int id = (int) viewHolder.itemView.getTag();
-//                //remove from DB
-//                Consumption consumption = Consumption.findById(context, id);
-//                consumption.delete(context);
-//                //update the list
-//                mAdapter.swapCursor(Consumption.findAll(context));
-//            }
-//
-//        }).attachToRecyclerView(consumptionRecyclerView);
-
         getActivity().setTitle(CONSUMPTION);
 
         // Setting ViewPager for each Tabs
