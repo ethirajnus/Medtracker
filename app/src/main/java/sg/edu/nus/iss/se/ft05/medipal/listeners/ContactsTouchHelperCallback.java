224e68738767d79675e2bff7813be3a4a15bf6a8
Ashish Shrikant Katre <e0146812@nus.edu.sg>
2017-03-24 12:00:17 +0800

    refactoring

--- app/src/main/java/sg/edu/nus/iss/se/ft05/medipal/listeners/ContactsTouchHelperCallback.java
+++ app/src/main/java/sg/edu/nus/iss/se/ft05/medipal/listeners/ContactsTouchHelperCallback.java
@@ -2,10 +2,6 @@ package sg.edu.nus.iss.se.ft05.medipal.listeners;
 
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.view.View;
-import android.widget.Toast;
-
-import org.apache.http.conn.ConnectTimeoutException;
 
 import sg.edu.nus.iss.se.ft05.medipal.adapters.ItemTouchHelperAdapter;
 
@@ -13,19 +9,34 @@ import sg.edu.nus.iss.se.ft05.medipal.adapters.ItemTouchHelperAdapter;
  * class for implementing touch functionality
  * Created by ashish katre on 3/17/2017.
  */
-
 public class ContactsTouchHelperCallback extends ItemTouchHelper.Callback {
 
     private final ItemTouchHelperAdapter mAdapter;
-    private int dragfrom = -1;
-    private int dragto = -1;
+    public static final int ICE_MINUS_ONE = -1;
+    public static final int ICE_TEN = 10;
+    public static final int ICE_ZERO = 0;
+    private int dragfrom = ICE_MINUS_ONE;
+    private int dragto = ICE_MINUS_ONE;
 
+    /**
+     * Constructor
+     *
+     * @param adapter ItemTouchHelperAdapter
+     */
     public ContactsTouchHelperCallback(ItemTouchHelperAdapter adapter) {
 
         super();
         mAdapter = adapter;
     }
 
+    /**
+     * onmove of selected item
+     *
+     * @param contactsListAdapter
+     * @param contactsViewHolder
+     * @param contactsViewHolderTarget
+     * @return boolean
+     */
     @Override
     public boolean onMove(RecyclerView contactsListAdapter,
                           RecyclerView.ViewHolder contactsViewHolder,
@@ -34,8 +45,7 @@ public class ContactsTouchHelperCallback extends ItemTouchHelper.Callback {
         int fromPosition = contactsViewHolder.getAdapterPosition();
         int toPosition = contactsViewHolderTarget.getAdapterPosition();
 
-
-        if (dragfrom == -1) {
+        if (ICE_MINUS_ONE == dragfrom) {
 
             dragfrom = fromPosition;
         }
@@ -45,57 +55,73 @@ public class ContactsTouchHelperCallback extends ItemTouchHelper.Callback {
         return true;
     }
 
+    /**
+     * after movement completed
+     *
+     * @param recyclerView
+     * @param viewHolder
+     */
     @Override
     public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
         super.clearView(recyclerView, viewHolder);
 
-        if (dragfrom != -1 && dragto != -1 && dragfrom != dragto) {
+        if (ICE_MINUS_ONE != dragfrom && ICE_MINUS_ONE != dragto && dragfrom != dragto) {
 
             mAdapter.onItemMove(dragfrom, dragto);
         }
 
-        dragfrom = dragto = -1;
+        dragfrom = dragto = ICE_MINUS_ONE;
     }
 
-/*    @Override
-    public boolean isLongPressDragEnabled() {
-
-        return true;
-    }*/
-
-
+    /**
+     * interpolate Out Of Bounds Scroll
+     *
+     * @param recyclerView
+     * @param viewSize
+     * @param viewSizeOutOfBounds
+     * @param totalSize
+     * @param msSinceStartScroll
+     * @return int
+     */
     @Override
     public int interpolateOutOfBoundsScroll(RecyclerView recyclerView, int viewSize, int viewSizeOutOfBounds, int totalSize, long msSinceStartScroll) {
 
         final int direction = (int) Math.signum(viewSizeOutOfBounds);
-        return 10 * direction;
+        return (ICE_TEN * direction);
     }
 
+    /**
+     * is ItemView Swipe Enabled
+     *
+     * @return boolean
+     */
     @Override
     public boolean isItemViewSwipeEnabled() {
 
         return true;
     }
 
+    /**
+     * get movement flag
+     *
+     * @param recyclerView
+     * @param contactsViewHolder
+     * @return int
+     */
     @Override
     public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder contactsViewHolder) {
 
-     /*   int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
-        int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
-
-        return makeMovementFlags(dragFlags, swipeFlags);*/
-
-//        return makeFlag(ItemTouchHelper.ACTION_STATE_DRAG,
-//                ItemTouchHelper.DOWN | ItemTouchHelper.UP);
-
-//        return makeFlag(ItemTouchHelper.ACTION_STATE_IDLE, ItemTouchHelper.UP | ItemTouchHelper.DOWN) | makeFlag(ItemTouchHelper.ACTION_STATE_DRAG,
-//                ItemTouchHelper.DOWN | ItemTouchHelper.UP);
-
         int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN | ItemTouchHelper.START | ItemTouchHelper.END;
-        int swipeFlags = 0;
+        int swipeFlags = ICE_ZERO;
         return makeMovementFlags(dragFlags, swipeFlags);
     }
 
+    /**
+     * process on swiped
+     *
+     * @param viewHolder
+     * @param direction
+     */
     @Override
     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
 
